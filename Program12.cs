c743511e28a2479b25917a9dd2c45371ba7a1064
pipichaan <147584671+pipichaan@users.noreply.github.com>
2023-12-22 01:04:35 +0300

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("укажите путь"); //E:\\DOCUMENT\\12.txt
+string path = Console.ReadLine();
+string path1 = Directory.GetCurrentDirectory();
+if (File.Exists(path1))
+{
+    string a = File.ReadAllText(path);
+    File.WriteAllText(path1 + "\\12.txt", a);
+    Console.WriteLine(path1 + "\\12.txt");
+}
+else
+{
+    Console.WriteLine("ошибка");
+}
